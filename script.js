d26fa82d85372fb43b1160ce9d77d806c2e9b1a9
Kalpanabharath <stonepanakeerthu@gmail.com>
2024-03-12 21:23:55 +0530

    FEAT:html,css,js complete

---
+++ script.js
@@ -0,0 +1,41 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=imperial
+let apikey='597acadb714c2bcca1d1130d341dae5b';
+let resel=document.getElementById('result');
+
+
+function getWeatherData(city){
+
+    let url='https://api.openweathermap.org/data/2.5/weather';
+    let fullurl=`${url}?q=${city}&appid=${apikey}&units=imperial`;
+    let weatherpro=fetch(fullurl);
+
+
+    return weatherpro.then((response)=>{
+        return response.json()
+    })
+
+}
+function search(){
+    let city=document.getElementById("cityname").value ;
+    getWeatherData(city)
+    .then((response)=>{
+        console.log(response)
+        showweather(response)
+
+    })
+    .catch((err)=>{
+        console.log(err)
+
+    })
+    resel.style.visibility="visible";
+
+}
+showweather=(watherdata)=>{
+    document.getElementById('location').innerHTML=watherdata.name;
+    document.getElementById('Weather').innerHTML=watherdata.weather[0].main;
+    document.getElementById('tempratorespan').innerHTML=watherdata.main.temp;
+    document.getElementById('max-tempratorespan').innerHTML=watherdata.main.temp_max;
+    document.getElementById('min-tempratorespan').innerHTML=watherdata.main.temp_min;
+
+
+}
